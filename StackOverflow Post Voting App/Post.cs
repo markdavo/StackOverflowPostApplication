67a0c8e61d3dac7e9f197df0795c93b0fc79505f
Mark Davidson <marko.davo@gmail.com>
2017-04-05 22:37:27 +1000

    Add project files.

---
+++ StackOverflow Post Voting App/Post.cs
@@ -0,0 +1,84 @@
+﻿using System;
+
+namespace StackOverflow_Post_Voting_App
+{
+    public class Post
+    {   //Note I have come from Java and have a habit of typing my variable types
+        private int id1;
+        private string title;
+        private string description1;
+        private DateTime dateAndTimeOfPost1;
+        private int numberOfVotes1 = 0;
+
+        //ctor
+        public Post(int id, string titleOfPost, string descriptionOfPost)
+        {
+            Setid(id);
+            SetTitle(titleOfPost);
+            Setdescription(descriptionOfPost);
+            SetnumberOfVotes(0);
+            SetdateAndTimeOfPost(DateTime.Now); 
+        }
+
+        //methods
+        public void UpVoting()
+        {
+            SetnumberOfVotes(GetnumberOfVotes() + 1); 
+        }
+        public void DownVoting()
+        {
+            SetnumberOfVotes(GetnumberOfVotes() - 1); 
+        }
+
+        //Getters and setters. Note that some are private. 
+        public string GetTitle()
+        {
+            return title;
+        }
+
+        private void SetTitle(string value)
+        {
+            title = value;
+        }
+
+        public int Getid()
+        {
+            return id1;
+        }
+
+        private void Setid(int value)
+        {
+            id1 = value;
+        }
+
+        public string Getdescription()
+        {
+            return description1;
+        }
+
+        private void Setdescription(string value)
+        {
+            description1 = value;
+        }
+
+        public DateTime GetdateAndTimeOfPost()
+        {
+            return dateAndTimeOfPost1;
+        }
+
+        private void SetdateAndTimeOfPost(DateTime value)
+        {
+            dateAndTimeOfPost1 = value;
+        }
+
+        public int GetnumberOfVotes()
+        {
+            return numberOfVotes1;
+        }
+
+        private void SetnumberOfVotes(int value)
+        {
+            numberOfVotes1 = value;
+        }
+    }
+}
---
+++ StackOverflow Post Voting App/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflow_Post_Voting_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Would you like to create a post (Type 'yes' or 'no'): ");
+            string willCreatePost = Console.ReadLine();
+            var postnumber = new List<Post>();
+            int id = 1; 
+
+            while (willCreatePost.ToLower() == "yes")
+            {
+                Console.WriteLine("Please type a heading for your post: ");
+                string tempTitleOfPost = Console.ReadLine();
+                Console.WriteLine("Please type a description of your post: ");
+                string tempDescriptionOfPost = Console.ReadLine();
+
+                postnumber.Add(new Post(id, tempTitleOfPost, tempDescriptionOfPost));
+                id += 1; 
+                Console.WriteLine("Would you like to create another post (Type 'yes' or 'no'): ");
+                willCreatePost = Console.ReadLine();
+            }
+
+            Console.WriteLine("You now need to vote on your post");
+            Console.WriteLine("The following are your posts:");
+
+            for (var i = 0; i < postnumber.Count; i++)
+            {
+                Console.WriteLine(postnumber[i].Getid() + " " + postnumber[i].GetTitle());
+            }           
+            Console.Write("Please up or down vote on your posts by typing the post number and '+' or '-'" +
+                "or hit enter finish: ");
+            var inputVotes = Console.ReadLine();
+
+            while (!String.IsNullOrWhiteSpace(inputVotes))
+            {
+                char[] PostAndVotes = inputVotes.ToCharArray();
+                int postNo = Convert.ToInt32(PostAndVotes[0].ToString());
+                var postVote = PostAndVotes[1].ToString();
+
+                for (var i = 0; i < postnumber.Count; i++)
+                {
+                    if (postNo == postnumber[i].Getid())
+                    {
+                        if (postVote == "+")
+                        {
+                            postnumber[i].UpVoting();
+                        }
+                        else if (postVote == "-")
+                        {
+                            postnumber[i].DownVoting();
+                        }
+                        else
+                        {
+                            Console.WriteLine("That was not a valid entry");
+                        }
+                    }
+                    Console.Write("Please up or down vote on your posts again by typing the post number and '+' or '-'" +
+                "or hit enter finish: ");
+                    inputVotes = Console.ReadLine();
+                }
+            }
+            Console.WriteLine("Summary of posts with their votes");
+            Console.WriteLine("*****************");
+            for (var i = 0; i < postnumber.Count; i++)
+            {
+                Console.WriteLine("{0} {1} post has {2} votes", postnumber[i].Getid(), postnumber[i].GetTitle(), postnumber[i].GetnumberOfVotes());
+                Console.WriteLine(postnumber[i].Getdescription());
+                Console.WriteLine("*****************");
+            }
+        }
+    }
+}
